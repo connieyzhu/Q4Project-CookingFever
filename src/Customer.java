933683c0a78972944671360fefc4a0ed801f4ec8
Jennifer Zhu <90798447+JZ101@users.noreply.github.com>
2022-04-04 12:45:39 -0700

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,1 +1,87 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.MouseInfo;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+import java.awt.Image;
+
+public class Customer {
+	private double x, y; //position
+	private String name;
+	private Image img; 	
+	private AffineTransform tx; 
+
+	public Customer(double x, double y, String custName) {
+		this.x = x; 
+		this.y = y;
+		name = custName;
+		if(name == "Daphne") {
+			img = getImage("/imgs/D1.png"); 
+		}
+		if(name == "Kyle") {
+			img = getImage("/imgs/K1.png"); 
+		}
+		if(name == "Francis") {
+			img = getImage("/imgs/F1.png"); 
+		}
+		if(name == "Linda") {
+			img = getImage("/imgs/L1.png"); 
+		}
+
+		tx = AffineTransform.getTranslateInstance(x, y);
+		init(x, y); 				//initialize the location of the image
+									//use your variables
+	}
+	
+	public void changePicture(String newFileName) {
+		img = getImage(newFileName);
+		init(x, y);
+	}
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+
+		//call update to update the actually picture location
+		update();
+		g2.drawImage(img, tx, null);
+		
+
+	}
+
+	/* update the picture variable location */
+	private void update() {
+
+		tx.setToTranslation(x, y);
+
+		//to scale it up or down to change size, .5 means 50% of original file
+		tx.scale(0.4, 0.4);
+		
+	}
+
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		
+		//to scale it up or down to change size, .5 means 50% of original file
+		tx.scale(0.4, 0.4);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Background.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+}
+
 
