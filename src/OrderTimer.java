8d9fc493a4881084c23c9905bcf256542d5a1450
Jennifer Zhu <90798447+JZ101@users.noreply.github.com>
2022-04-05 14:23:22 -0700

    Update OrderTimer.java

--- src/OrderTimer.java
+++ src/OrderTimer.java
@@ -10,76 +10,49 @@ import java.awt.Toolkit;
 import java.awt.geom.AffineTransform;
 import java.net.URL;
 import java.awt.Image;
+import java.util.Timer;
 
 public class OrderTimer {
-	private int x, y, gx, gy; //position
-	private Image img; 	
-	private AffineTransform tx; 
-
+	private int x, y, gx, gy, tx, ty; //position
+	//private Image img; 	
+	//private AffineTransform tx; 
+	
 	public OrderTimer(int x, int y) {
 		this.x = x; 
 		this.y = y;
 		gx = x;
 		gy = y;
-		//vy = -1;
-
-		//tx = AffineTransform.getTranslateInstance(x, y);
-		//init(x, y); 				//initialize the location of the image
-									//use your variables
-	}
-	
-	public void changePicture(String newFileName) {
-		img = getImage(newFileName);
-		init(x, y);
+		tx = x-190;
+		ty = y + 150;
 	}
 	
 	public void paint(Graphics g) {
 		//these are the 2 lines of code needed draw an image on the screen
 		Graphics2D g2 = (Graphics2D) g;
 		
-		g.setColor(Color.gray); //timer box
-		g.fillRect(gx, gy, x, y);
+		g.setColor(Color.gray);
+		g.fillRect(gx, gy, tx, gy+150);
 		
-		g.setColor(Color.green); //timer box
-		g.fillRect(x, y, x+1, y+55);
+		g.setColor(Color.green);
+		g.fillRect(x, y, tx, ty);
 		
-		//call update to update the actually picture location
 		update();
-		//g2.drawImage(img, tx, null);
 		
-
 	}
 
 	/* update the picture variable location */
 	private void update() {
 		
-		y += 1;
-		
-		//tx.setToTranslation(x, y);
-
-		//to scale it up or down to change size, .5 means 50% of original file
-		//tx.scale(0.4, 0.4);
-		
-	}
-
-	
-	private void init(double a, double b) {
-		tx.setToTranslation(a, b);
-		
-		//to scale it up or down to change size, .5 means 50% of original file
-		tx.scale(0.4, 0.4);
-	}
-
-	private Image getImage(String path) {
-		Image tempImage = null;
-		try {
-			URL imageURL = Background.class.getResource(path);
-			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
-		} catch (Exception e) {
-			e.printStackTrace();
+		if(ty <= gy+150) {
+			y += 1;
+			ty -= 1;
+		}else {
+			y = gy+150;
 		}
-		return tempImage;
+		
+		
 	}
+
 }
 
 
