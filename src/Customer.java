18638491a2febbeae5a8f0018fe142b0a904abf8
Jennifer Zhu <90798447+JZ101@users.noreply.github.com>
2022-04-20 13:11:28 -0700

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -12,29 +12,29 @@ import java.net.URL;
 import java.awt.Image;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Customer {
-	private int x1, x, y, num; //position
+	private double x1, x, y, totalTime; //position
 	private int sec = 1600;
-	private int vx = -2;
+	private double vx = -0.5;
+	private double leavingVx = -0.01;
 	private String name;
+	private boolean ready, done;
 	private Image img; 	
 	private AffineTransform tx; 
 	Timer timer;
 	OrderTimer custTimer;
 	Background order;
-	Customer cust;
-	Position pos;
+	Position pos = new Position("wait");
 	
 	public Customer(int x, int y, String custName) {
-		x1 = x+1200;
+		x1 = pos.getWait();
 		this.x = x; 
 		this.y = y;
+		ready = false;
+		done = false;
+		totalTime = 0;
 		name = custName;
-		timer = new Timer();
-		num = ThreadLocalRandom.current().nextInt(0, 8);
-		
 		if(name == "Daphne") {
 			img = getImage("/imgs/D1.png"); 
 		}
@@ -51,10 +51,10 @@ public class Customer {
 		tx = AffineTransform.getTranslateInstance(x1, y);
 		init(x1, y); 				//initialize the location of the image
 									//use your variables
-		order = new Background(x-80, 70, "/imgs/Order Bubble.png");
-		custTimer = new OrderTimer(x+10, 75, 1);
-		pos = new Position("Person");
-	
+		//order = new Background(x-80, 70, "/imgs/Order Bubble.png");
+		//custTimer = new OrderTimer(x+10, 75, 1);
+		timer = new Timer();
+		//custTimer = new OrderTimer();
 	}
 	
 	public void changePicture(String newFileName) {
@@ -65,16 +65,38 @@ public class Customer {
 	public void paint(Graphics g) {
 		//these are the 2 lines of code needed draw an image on the screen
 		Graphics2D g2 = (Graphics2D) g;
-		timer.schedule(new Wait(), 500*num);
+		timer.schedule(new RemindTask(), sec);
 		//call update to update the actually picture location
 		g2.drawImage(img, tx, null);
 		
-		if(x1 == x) {
-			//custTimer = new OrderTimer(x+10, 75, 1);
-			custTimer.paint(g);
-			order.paint(g);
-		}
+		update();
 		
+		/*if(ready) {
+			//custTimer = new OrderTimer(x+10, 75, 1);
+			//custTimer.paint(g);
+			order.paint(g);
+		}*/
+
+	}
+	
+	public boolean getReady() {
+		return ready;
+	}
+	
+	public double getX1() {
+		return x1;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getVX() {
+		return leavingVx;
+	}
+	
+	public void setX1(double x) {
+		x1 = x;
 	}
 	
 	/* update the picture variable location */
@@ -108,26 +130,27 @@ public class Customer {
 	
 	class RemindTask extends TimerTask {
 	    public void run() {
-	    	if(x1 == x && custTimer.getTime()) {
-	    		System.out.println("Customer Arrived!");
-	    	}else if(!custTimer.getTime()) {
-    			x1 += vx;
-    			update();
-    			timer.schedule(new RemindTask(), sec);
-    		}else {
+	    	if(x1 == x-2000) {
+	    		System.out.println("cust left");
+	    	}else if(totalTime >= 90742){//totalTime >= 90742){
+	    		x1 += vx;
+	    		ready = false;
+	    		done = true;
+				update();
+				sec = 30000;
+				timer.schedule(new RemindTask(), sec);
+	    	}else if(x1 == x) {
+	    		totalTime++;
+				System.out.println(totalTime);
+	    		ready = true;
+	    		timer.schedule(new RemindTask(), sec);
+	    	}else {
 				x1 += vx;
 				update();
 				timer.schedule(new RemindTask(), sec);
 			}
 	    }
 	}
-	
-	class Wait extends TimerTask {
-	    public void run() {
-	    	timer.schedule(new RemindTask(), sec);
-	    }
-	}
-	
 }
 
 
