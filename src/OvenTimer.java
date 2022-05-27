efb97a09564ce1a472bc5de8684c17536dcd2077
connieyzhu <60797237+connieyzhu@users.noreply.github.com>
2022-05-26 22:37:57 -0700

    Update OvenTimer.java

--- src/OvenTimer.java
+++ src/OvenTimer.java
@@ -2,16 +2,19 @@ import java.util.Timer;
 import java.util.TimerTask;
 public class OvenTimer {
 	Timer timer;
+	protected int index;
 
-    public OvenTimer(int seconds) {
+    public OvenTimer(int seconds, int ind) {
         timer = new Timer();
+        index = ind;
         timer.schedule(new RemindTask(), seconds*1000); // schedule the task
     }
 
     class RemindTask extends TimerTask {
         public void run() {
             for(int i = 3; i < Runner.getObjectList().size(); i++) {
-            	Runner.getObject(i).bakeChange(); 
+            	Runner.getObject(index).bakeChange(); 
+            	
             }
             timer.cancel(); //Terminate the timer thread
         }
